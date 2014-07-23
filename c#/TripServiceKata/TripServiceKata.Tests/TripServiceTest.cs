e201e66bf1c671e8cf094a5b2b3bb0d4c257f222
Tomasz Pamuła <tomasz.p.pamula@gmail.com>
2014-07-23 18:54:31 +0200

    Added a test for user without friends.

--- c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -1,6 +1,8 @@
-﻿using TripServiceKata.Exception;
+﻿using System;
+using TripServiceKata.Exception;
 using TripServiceKata.Trip;
 using Xunit;
+using KataTrip = TripServiceKata.Trip.Trip;
 using KataUser = TripServiceKata.User.User;
 
 namespace TripServiceKata.Tests
@@ -8,7 +10,26 @@ namespace TripServiceKata.Tests
     public class TripServiceTest
     {
         [Fact]
-        public void should_throw_error_when_user_not_logged_in()
+        public void returns_no_trips_when_user_has_no_friends()
+        {
+            var user = new KataUser();
+
+            var userWithoutFriends = new KataUser();
+            userWithoutFriends.AddTrip(new KataTrip());
+            userWithoutFriends.AddTrip(new KataTrip());
+
+            var someFriend = new KataUser();
+            someFriend.AddTrip(new KataTrip());
+            someFriend.AddTrip(new KataTrip());
+            user.AddFriend(someFriend);
+
+            var tripService = new TestableTripService(user);
+
+            Assert.Equal(0, tripService.GetTripsByUser(userWithoutFriends).Count);
+        }
+
+        [Fact]
+        public void should_throw_exception_when_user_not_logged_in()
         {
             KataUser guest = null;
             var tripService = new TestableTripService(guest);
